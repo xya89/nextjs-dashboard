b0c6ccd372636b9230f03c166b060c88c86566b3
xya89 <xya89@sfu.ca>
2024-05-12 12:16:30 -0700

    Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap' })
+export const lusitana = Lusitana({ weight: "400", subsets: ['latin'], style: 'normal' })
