15f39428d7f2522ef93faf8326ecd0bf07ba92dc
xya89 <xya89@sfu.ca>
2024-05-13 17:07:24 -0700

    Steaming Latest Invoices Components

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,15 +3,18 @@ import RevenueChart from '../../ui/dashboard/revenue-chart'
 import LatestInvoices from '../../ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts'
 // import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../../lib/data'
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'
+// import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'
+import { fetchCardData } from '@/app/lib/data'
 
 import { Suspense } from 'react'
-import { RevenueChartSkeleton } from '@/app/ui/skeletons'
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons'
+
+
 
 export default async function Page() {
     // const revenue = await fetchRevenue();
+    // const latestInvoices = await fetchLatestInvoices();
 
-    const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfInvoices,
         numberOfCustomers,
@@ -38,7 +41,11 @@ export default async function Page() {
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
                 </Suspense>
-                {<LatestInvoices latestInvoices={latestInvoices} />}
+
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+
             </div>
         </main>
     )
