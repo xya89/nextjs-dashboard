01bc831835cec936ee55a677fdf303b0ae99d7e8
xya89 <xya89@sfu.ca>
2024-05-13 17:40:42 -0700

    Debounced Search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,14 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathName = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Seaching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathName}?${params.toString()}`)
-  }
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
